05718e238aa3038d62b9f56c3781884eb707d6a4
Emily Mamcarz <emily.mcrz@gmail.com>
2025-09-20 22:18:55 -0400

    Completed Activity 1.4 - Chapter 4 - adding email input

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Regexp
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -14,7 +14,12 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your email?', validators=[
+            DataRequired(),
+            Regexp(r'.+@.+', message="Please include an '@' in the email address."),
+        ])
     submit = SubmitField('Submit')
+
 
 
 @app.errorhandler(404)
@@ -34,6 +39,24 @@ def index():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+
         session['name'] = form.name.data
+
+        # Only save email if it contains "utoronto"
+        if "utoronto" in form.email.data:
+            session['email'] = form.email.data
+        else:
+            session['email'] = ""
+
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+
+    return render_template(
+        'index.html',
+        form=form,
+        name=session.get('name'),
+        email=session.get('email'),
+    )
